6123e2549afa401df02482740723a382eb4e197b
arsenaid <arsenaid1974@gmail.com>
2025-12-22 16:04:44 -0800

    fix: label header display

--- src/components/projections/ProjectionChart.tsx
+++ src/components/projections/ProjectionChart.tsx
@@ -124,7 +124,7 @@ export function ProjectionChart({
         <ResponsiveContainer width="100%" height="100%">
           <ComposedChart
             data={chartData}
-            margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
+            margin={{ top: 20, right: 10, left: 0, bottom: 0 }}
           >
             <CartesianGrid
               strokeDasharray="3 3"
